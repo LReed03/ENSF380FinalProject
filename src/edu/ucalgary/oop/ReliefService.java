469306e8ee2c7178e3090e2072ac355db5ac6c71
Landon Reed <144170201+LReed03@users.noreply.github.com>
2025-03-18 18:27:15 -0600

    Finished reliefservice class

--- src/edu/ucalgary/oop/ReliefService.java
+++ src/edu/ucalgary/oop/ReliefService.java
@@ -1,5 +1,57 @@
 package edu.ucalgary.oop;
 
 public class ReliefService{
-    
+    private Person inquirer;
+    private Person missingPerson;
+    private String dateOfInquiry;
+    private String infoProvided;
+    private Location lastKnownLocation;
+
+    public ReliefService(Person inquirer, Person missingPerson, String dateOfInquiry, Sting infoProvided, Location lastKnownLocation){
+        this.inquirer = inquirer;
+        this.missingPerson = missingPerson;
+        this.dateOfInquiry = dateOfInquiry;
+        this.infoProvided = infoProvided;
+        this.lastKnownLocation = lastKnownLocation;
+    }
+
+    public Person getInquirer(){
+        return this.inquirer;
+    }
+
+    public void setInquirer(Person inquirer){
+        this.inquirer = inquirer;
+    }
+
+    public void setMissingPerson(Person person){
+        this.missingPerson = person;
+    }
+
+    public Person getMissingPerson(){
+        return this.missingPerson;
+    }
+
+    public void setDateOfInquiry(){
+        this.dateOfInquiry = dateOfInquiry;
+    }
+
+    public String getDateOfInquiry(){
+        return this.dateOfInquiry;
+    }
+
+    public void setInfoProvided(String info){
+        this.infoProvided = info;
+    }
+
+    public String getInfoProvided(){
+        return this.infoProvided;
+    }
+
+    public Location getLastKnownLocation(){
+        return this.lastKnownLocation;
+    }
+
+    public void setLastKnownLocation(){
+        this.lastKnownLocation = lastKnownLocation;
+    }
 }
